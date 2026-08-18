312b39269be31818d21c28b3f08ca7efce005c4b
Micahel Biru <mdawit384@gmail.com>
2026-08-18 19:45:23 +0300

    remove dependecies and genrated files

--- backend/dist/src/routes/dashboard.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const dashboard_controller_1 = require("../controllers/dashboard.controller");
-const router = (0, express_1.Router)();
-router.get("/stats", auth_middleware_1.authMiddleware, dashboard_controller_1.getDashboardStats);
-exports.default = router;
